000422aa461512a7fcf927d4599bc8f6fa350b1a
justinthelaw <justin.law@defenseunicorns.com>
2024-04-16 11:25:24 -0400

    chore: re-org dirs, build.sh enhance, rc handling

--- build.sh
+++ build.sh
@@ -2,12 +2,13 @@
 
 set -euxo pipefail
 
+DOCKERFILE=${DOCKERFILE:="./docker/Dockerfile"}
 K3S_TAG=${K3S_TAG:="v1.28.8-k3s1"} # replace + with -, if needed
-CUDA_TAG=${CUDA_TAG:="12.4.1-base-ubuntu22.04"} # replace + with -, if needed
+RC_TAG=${RC_TAG:="rc.1"} # identifies release candidate (non-stable) upgrades
 
 IMAGE_REGISTRY=${IMAGE_REGISTRY:="ghcr.io/justinthelaw"}
 IMAGE_REPOSITORY=${IMAGE_REPOSITORY:="k3d-gpu-support"}
-IMAGE_TAG="$K3S_TAG-cuda-$CUDA_TAG"
+IMAGE_TAG="$K3S_TAG-$RC_TAG"
 IMAGE=${IMAGE:="$IMAGE_REGISTRY/$IMAGE_REPOSITORY:$IMAGE_TAG"}
 
 echo "IMAGE=$IMAGE"
@@ -24,7 +25,8 @@ docker buildx build \
   --build-arg K3S_TAG=$K3S_TAG \
   --platform linux/amd64,linux/arm64 \
   -t $IMAGE \
-  --push .
+  --push \
+  -f ${DOCKERFILE} . # should be the path e.g., ./docker/Dockerfile.rc
 
 docker buildx rm multiplatform
 
